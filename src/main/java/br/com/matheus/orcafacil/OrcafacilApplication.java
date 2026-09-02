a72503274c9aaa728b304aa2c9ee99ccede7a115
Matheus Medeiros <promatheusmedeiros@gmail.com>
2026-09-02 16:24:00 -0300

    chore: add initial springboot setup

---
+++ src/main/java/br/com/matheus/orcafacil/OrcafacilApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheus.orcafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcafacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcafacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheus/orcafacil/OrcafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.orcafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcafacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
